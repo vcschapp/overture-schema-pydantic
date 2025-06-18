52c1837f8c3cbe2d2d6817e53816dd5e7b358f19
schapper <schapper@amazon.com>
2025-06-18 12:34:41 -0700

    Create a simple "Overture feature ID" type

---
+++ src/overture_schema_pydantic/id.py
@@ -0,0 +1,14 @@
+from typing import Annotated
+from pydantic import Field
+
+Id = Annotated[
+    str,
+    Field(
+        min_length=1,
+        pattern=r'^(\S.*)?\S$',
+        description="A feature ID. This may be an ID associated with "
+                    "the Global Entity Reference System (GERS) "
+                    "if-and-only-if the feature represents an entity "
+                    "that is part of GERS."
+    )
+]
---
+++ tests/test_id.py
@@ -0,0 +1,24 @@
+from overture_schema_pydantic.id import Id
+
+import pytest
+from pydantic import BaseModel, ValidationError
+
+class DummyModel(BaseModel):
+    id: Id
+
+def test_valid():
+    m = DummyModel(id="foo")
+    assert m.id == "foo"
+
+def test_empty():
+    with pytest.raises(ValidationError):
+        DummyModel(id="")
+
+def test_blank():
+    with pytest.raises(ValidationError):
+        DummyModel(id=" ")
+
+
+def test_whitespace():
+    with pytest.raises(ValidationError):
+        DummyModel(id=" foo ")
